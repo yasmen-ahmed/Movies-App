5a38390631f12b2ab92dd871f1f78851c3f05238
yasmen-ahmed <yasmenahmedmuhamed@gmail.com>
2024-07-31 19:07:24 +0300

    fetch data

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,28 @@
 import Image from "next/image";
+import Results from '@/components/Results'
 
-export default function Home() {
+const API_KEY = process.env.API_KEY
+
+
+
+
+export default async function Home({searchParams}) {
+  const genre =searchParams.genre || 'fetchTrending'
+
+  const res = await fetch(`https://api.themoviedb.org/3${
+    genre === 'fetchTopRated'? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&languages=en-US&page=1`)
+
+    const data = await res.json()
+    if(!res.ok){
+      throw new Error('Failed to fetch data')
+    }
+
+    const results = data.results 
+  
+
   return (
      <div>
-      Hi
+      <Results results={results}/>
      </div>
   );
 }
